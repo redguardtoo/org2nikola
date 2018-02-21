db63b198ddbe79ba43c618bd90c4767776ac03da
Chen Bin <chenbin.sh@gmail.com>
2018-02-21 14:14:09 +1100

    No drawer exporting (which is default in Emacs 25+)

--- org2nikola-pkg.el
+++ org2nikola-pkg.el
@@ -1,2 +1,2 @@
-(define-package "org2nikola" "0.1.5"
+(define-package "org2nikola" "0.1.6"
                 "export html and meta data used by static blog like nikola from org file")
--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pkg=org2nikola-0.1.5
+pkg=org2nikola-0.1.6
 mkdir $pkg
 cp README.org $pkg
 cp *.el $pkg
