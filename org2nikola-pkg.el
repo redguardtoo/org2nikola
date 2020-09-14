ce10e3715319d97853c1f4574eed897f82be199f
Chen Bin <chenbin.sh@gmail.com>
2020-09-15 00:02:15 +1000

    clean code&doc

--- org2nikola-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "org2nikola" "0.2.0"
-                "export html and meta data used by static blog like nikola from org file")
--- pkg.sh
+++ pkg.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-pkg=org2nikola-0.2.0
+name=org2nikola
+version=0.2.1
+pkg=$name-$version
 mkdir $pkg
 cp README.org $pkg
 cp *.el $pkg
+cat << EOF > $pkg/$name-pkg.el
+(define-package "$name" "$version" "Conver Org to HTML for static blog generator Nikola")
+EOF
+
 if [[ `uname -s` == *Darwin* ]]; then
    COPYFILE_DISABLE="" tar cvf $pkg.tar $pkg/
 else
    tar cvf $pkg.tar $pkg/
 fi
 rm -rf $pkg/
-
